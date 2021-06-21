72516ab5a9228196f7e2a2b3a514a284910fd7a4
Rankz <ianmichaels3@live.co.uk>
2021-06-21 16:39:21 +0100

    test: check paramenter value on press

--- client/pressCallback.test.js
+++ client/pressCallback.test.js
@@ -2,17 +2,17 @@ import { Text } from "react-native";
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 
-export default function Component({ handlePress }) {
-  return <Text onPress={handlePress}>This is whatever!</Text>;
+export default function Component({ handlePress, name }) {
+  return <Text onPress={() => handlePress(name)}>This is whatever!</Text>;
 }
 
 describe("pressCallback", () => {
   it("should fire callback on press", () => {
     const mockHandler = jest.fn();
-    const screen = render(<Component handlePress={mockHandler} />);
+    const screen = render(<Component handlePress={mockHandler} name="Ian" />);
     expect(mockHandler).toHaveBeenCalledTimes(0);
     const textElement = screen.getByText(/whatever/i);
     fireEvent.press(textElement);
-    expect(mockHandler).toHaveBeenCalledTimes(1);
+    expect(mockHandler).toHaveBeenLastCalledWith("Ian");
   });
 });
