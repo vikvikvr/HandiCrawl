f1cab0d0761e4ffd6e6c9117b3857290d4b79d89
Viktor <ricchiuto.viktor@gmail.com>
2021-06-21 11:06:37 +0200

    test: add basic test for SendButton

---
+++ client/sheets/EditNewMarkerInfo/SendButton.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SendButton } from "./SendButton";
+import { render } from "@testing-library/react-native";
+
+describe("SendButton", () => {
+  it("should render without throwing", () => {
+    render(<SendButton />);
+  });
+  it('should show a "send" text ', () => {
+    const { getByText } = render(<SendButton />);
+    getByText("Send");
+  });
+});
