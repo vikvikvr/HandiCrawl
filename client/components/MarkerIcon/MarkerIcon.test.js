02e61babf09cfb80aecd50c38e1fcbdcabc40896
Viktor <ricchiuto.viktor@gmail.com>
2021-06-21 20:05:08 +0200

    test: press callback for MarkerIcon

--- client/components/MarkerIcon/MarkerIcon.test.js
+++ client/components/MarkerIcon/MarkerIcon.test.js
@@ -1,10 +1,18 @@
 import "react-native";
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 import { MarkerIcon } from "./MarkerIcon";
 
 describe("MarkerIcon", () => {
   it("should render without throwing error", () => {
     render(<MarkerIcon />);
+  });
+  it("should respond to press events", () => {
+    const pressHandler = jest.fn();
+    const screen = render(<MarkerIcon onPress={pressHandler} />);
+    expect(pressHandler).toHaveBeenCalledTimes(0);
+    const button = screen.getByTestId("marker-icon");
+    fireEvent.press(button);
+    expect(pressHandler).toHaveBeenCalledTimes(1);
   });
 });
