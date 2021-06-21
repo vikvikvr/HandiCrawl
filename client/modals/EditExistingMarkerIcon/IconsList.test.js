a7c90edfdbcce5d3b5c85dc9d1d2ec07be506e13
Rankz <ianmichaels3@live.co.uk>
2021-06-21 15:43:45 +0100

    test: number of items renderd by the list

--- client/modals/EditExistingMarkerIcon/IconsList.test.js
+++ client/modals/EditExistingMarkerIcon/IconsList.test.js
@@ -2,9 +2,16 @@ import "react-native";
 import React from "react";
 import { render } from "@testing-library/react-native";
 import { IconsList } from "./IconsList";
+import { allIcons } from "../../services/iconFactory";
 
 describe("IconsList", () => {
   it("should render without throwing error", () => {
     render(<IconsList />);
+  });
+
+  it("should render one marker for each icon", () => {
+    const document = render(<IconsList />);
+    const buttons = document.getAllByTestId("marker-icon");
+    expect(buttons).toHaveLength(allIcons.length);
   });
 });
