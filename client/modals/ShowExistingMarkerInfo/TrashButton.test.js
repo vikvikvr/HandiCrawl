cd732ed34624773cc260e86b7b9cd5cb7e876f57
Rankz <ianmichaels3@live.co.uk>
2021-06-21 17:06:03 +0100

    test:  press callback coming from imports

--- client/modals/ShowExistingMarkerInfo/TrashButton.test.js
+++ client/modals/ShowExistingMarkerInfo/TrashButton.test.js
@@ -1,10 +1,21 @@
 import "react-native";
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 import { TrashButton } from "./TrashButton";
+import * as markerService from "../../services/markerService";
+
+const deleteMarkerSpy = jest.spyOn(markerService, "deleteMarker");
 
 describe("TrashButton", () => {
   it("should render without throwing error", () => {
     render(<TrashButton />);
+  });
+
+  it("should fire callback on press", () => {
+    const screen = render(<TrashButton />);
+    expect(deleteMarkerSpy).toHaveBeenCalledTimes(0);
+    const buttonElement = screen.getByTestId("touchable-wrapper");
+    fireEvent.press(buttonElement);
+    expect(deleteMarkerSpy).toHaveBeenCalledTimes(1);
   });
 });
