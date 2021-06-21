93a40e54d6b27ac703c6b2607b4e7eb02951b4f2
Viktor <ricchiuto.viktor@gmail.com>
2021-06-21 19:03:05 +0200

    test: callback on press for send button

--- client/sheets/EditNewMarkerInfo/SendButton.test.js
+++ client/sheets/EditNewMarkerInfo/SendButton.test.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { SendButton } from "./SendButton";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
+import * as markerService from "../../services/markerService";
+
+const saveNewMarkerSpy = jest.spyOn(markerService, "saveNewMarker");
+const buttonText = /send/i;
 
 describe("SendButton", () => {
   it("should render without throwing", () => {
@@ -8,6 +12,13 @@ describe("SendButton", () => {
   });
   it('should show a "send" text ', () => {
     const { getByText } = render(<SendButton />);
-    getByText(/send/i);
+    getByText(buttonText);
+  });
+  it("should save a new marker on press", () => {
+    const { getByText } = render(<SendButton />);
+    expect(saveNewMarkerSpy).toHaveBeenCalledTimes(0);
+    const button = getByText(buttonText);
+    fireEvent.press(button);
+    expect(saveNewMarkerSpy).toHaveBeenCalledTimes(1);
   });
 });
