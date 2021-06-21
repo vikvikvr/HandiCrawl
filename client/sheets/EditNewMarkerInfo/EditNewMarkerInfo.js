13fb9fc326985f4a98e73b82c7ba04d0d8e89286
Viktor <ricchiuto.viktor@gmail.com>
2021-06-21 13:14:58 +0200

    fix: change import for bottom sheets

--- client/sheets/EditNewMarkerInfo/EditNewMarkerInfo.js
+++ client/sheets/EditNewMarkerInfo/EditNewMarkerInfo.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { BottomSheet, View } from "react-native";
+import { View } from "react-native";
+import { BottomSheet } from "react-native-btr";
 import { EditIconInfoHeader } from "./EditIconInfoHeader";
 import { EditPlaceNameInput } from "./EditPlaceNameInput";
 import { EditPlaceDescriptionInput } from "./EditPlaceDescriptionInput";
@@ -18,10 +19,10 @@ export function EditNewMarkerInfo() {
       onBackdropPress={() => setSheet("")}
     >
       <View style={styles.bottomAddIconView}>
-        <EditIconInfoHeader />
+        {/* <EditIconInfoHeader />
         <EditPlaceNameInput />
         <EditPlaceDescriptionInput />
-        <SendButton />
+        <SendButton /> */}
       </View>
     </BottomSheet>
   );
