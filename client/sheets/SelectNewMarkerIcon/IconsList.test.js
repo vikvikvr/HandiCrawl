87bc36c86079f902e5f5b5d7bf03b4ee1d98cfc7
Viktor <ricchiuto.viktor@gmail.com>
2021-06-21 15:07:28 +0200

    test: add press test to IconsList

--- client/sheets/SelectNewMarkerIcon/IconsList.test.js
+++ client/sheets/SelectNewMarkerIcon/IconsList.test.js
@@ -1,8 +1,11 @@
 import "react-native";
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 import { IconsList } from "./IconsList";
 import { allIcons } from "../../services/iconFactory";
+import * as markerService from "../../services/markerService";
+
+const pressHandler = jest.spyOn(markerService, "selectNewMarkerIcon");
 
 describe("IconsList", () => {
   it("should render without throwing", () => {
@@ -10,7 +13,15 @@ describe("IconsList", () => {
   });
   it("should render one marker for each icon", () => {
     const { getAllByTestId } = render(<IconsList />);
-    const found = getAllByTestId("marker-icon");
-    expect(found).toHaveLength(allIcons.length);
+    const buttons = getAllByTestId("marker-icon");
+    expect(buttons).toHaveLength(allIcons.length);
+  });
+  it("should pass iconName to press handler", () => {
+    const { getAllByTestId } = render(<IconsList />);
+    const buttons = getAllByTestId("marker-icon");
+    fireEvent.press(buttons[0]);
+    expect(pressHandler).toHaveBeenLastCalledWith(allIcons[0]);
+    fireEvent.press(buttons[3]);
+    expect(pressHandler).toHaveBeenLastCalledWith(allIcons[3]);
   });
 });
